d211a5e9b443077e16e3282d559fdfc51dff57ad
Julia Leonova <lyus20f145@student.bmstu.ru>
2021-12-05 18:01:44 +0300

    о

--- C_Lab_2/mainwindow.h
+++ C_Lab_2/mainwindow.h
@@ -2,13 +2,55 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QToolButton>
+#include <QLineEdit>
+#include <QGridLayout>
 
-class MainWindow : public QMainWindow
+class Button : public QToolButton
 {
     Q_OBJECT
 
 public:
-    MainWindow(QWidget *parent = nullptr);
-    ~MainWindow();
+    Button(const QString &text, QWidget *parent = 0);
+
+    QSize sizeHint() const;
 };
+
+class Calculator : public QMainWindow
+ {
+     Q_OBJECT
+
+ public:
+     Calculator(QWidget *parent = 0);
+
+ private slots:
+     void digitClicked();
+     void unaryOperatorClicked();
+     void additiveOperatorClicked();
+     void multiplicativeOperatorClicked();
+     void equalClicked();
+     void pointClicked();
+     void changeSignClicked();
+     void backspaceClicked();
+     void clear();
+     void clearAll();
+     void clearMemory();
+     void readMemory();
+     void setMemory();
+     void addToMemory();
+private:
+     Button *createButton(const QString &text, const char *member);
+     void abortOperation();
+     bool calculate(double rightOperand, const QString &pendingOperator);
+     double sumInMemory;
+     double sumSoFar;
+     double factorSoFar;
+     QString pendingAdditiveOperator;
+     QString pendingMultiplicativeOperator;
+     bool waitingForOperand;
+     QLineEdit *display;
+     enum { NumDigitButtons = 10 };
+     Button *digitButtons[NumDigitButtons];
+};
+
 #endif // MAINWINDOW_H
